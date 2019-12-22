1152771f3ad2f45bc9b3ee3a7039f655f9504229
camilasatie <camilasatie.dev@gmail.com>
2019-12-21 21:08:11 -0300

    Criando a pagina about

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Camila Satie</h1>
+    <h2>Desenvolvedora Front-End</h2>
+    <p>Relatando a minha transição de carreira para o mundo da tecnologia!</p>
+  </div>
+)
+
+export default Profile
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,7 +7,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Home</h1>
   </Layout>
 )
 
